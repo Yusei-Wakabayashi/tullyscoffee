4cc5e76042c2401296f24ec2a9abfac9e0168253
YuseiWakabayasi <fowakuma3@icloud.com>
2023-10-14 18:56:13 +0900

    routerのAPIの名前を書き換えました

--- ecsitesimulater/routes/web.php
+++ ecsitesimulater/routes/web.php
@@ -6,11 +6,11 @@ use App\Models\Item;
 Route::get('/', 'App\Http\Controllers\StartController@index');
 //データベース内のアイテム名
 Route::get('/item/start', 'App\Http\Controllers\StartController@getItems');
-Route::get('/item/search', 'App\Http\Controllers\StartController@searchItems');
+Route::get('/item/search/{name}', 'App\Http\Controllers\StartController@searchItems');
 Route::get('/item/world/{id}', 'App\Http\Controllers\StartController@selectWorld');
-Route::get('/item/world/all', 'App\Http\Controllers\StartController@worldlist');
+Route::get('/item/worldall', 'App\Http\Controllers\StartController@worldlist');
 Route::get('/item/categoly/{id}', 'App\Http\Controllers\StartController@selectCategoly');
-Route::get('/item/categoly/all', 'App\Http\Controllers\StartController@categolylist');
+Route::get('/item/categolyall', 'App\Http\Controllers\StartController@categolylist');
 Route::get('/item/recipe', 'App\Http\Controllers\StartController@findItemid');
 
 //Route::get('/{any?}', fn () => view('app'))->where('any', '.+');
